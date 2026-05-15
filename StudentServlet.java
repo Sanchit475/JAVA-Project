c2760ac509e251f16cb7af7bd275150169a19987
Sanchit475 <sanchit.yadav19april@gmail.com>
2026-05-15 12:18:27 +0530

    Update StudentServlet.java

--- StudentServlet.java
+++ StudentServlet.java
@@ -1,13 +1,13 @@
 package com.example;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
+import jakarta.servlet.RequestDispatcher;
 
 @WebServlet("/StudentServlet")
 public class StudentServlet extends HttpServlet {
@@ -25,15 +25,12 @@ public class StudentServlet extends HttpServlet {
         String gender = request.getParameter("gender");
         String address = request.getParameter("address");
 
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-
         try {
             Connection con =
                     DBConnection.getConnection();
 
             String sql =
-                    "INSERT INTO students(course,name,roll,semester,gender,address) VALUES(?,?,?,?,?,?)";
+                "INSERT INTO students(course,name,roll,semester,gender,address) VALUES(?,?,?,?,?,?)";
 
             PreparedStatement ps =
                     con.prepareStatement(sql);
@@ -48,15 +45,28 @@ public class StudentServlet extends HttpServlet {
             int result = ps.executeUpdate();
 
             if(result > 0){
-                out.println("<h1 style='color:green;text-align:center;'>Data Saved Successfully</h1>");
-            }else{
-                out.println("<h1 style='color:red;text-align:center;'>Failed</h1>");
+
+                // send data to next page
+                request.setAttribute("course", course);
+                request.setAttribute("name", name);
+                request.setAttribute("roll", roll);
+                request.setAttribute("semester", semester);
+                request.setAttribute("gender", gender);
+                request.setAttribute("address", address);
+
+                RequestDispatcher rd =
+                        request.getRequestDispatcher("success.jsp");
+
+                rd.forward(request, response);
+
+            } else {
+                response.getWriter().println("Submission Failed");
             }
 
             con.close();
 
         } catch(Exception e){
-            out.println(e);
+            response.getWriter().println(e);
         }
     }
 }
